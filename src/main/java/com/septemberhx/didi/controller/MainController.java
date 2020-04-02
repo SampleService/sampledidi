d56edaef51ab934cf30139c2b45e0282e95ae674
SeptemberHX <september_hx@outlook.com>
2020-04-02 20:59:32 +0800

    add: ver dep call

--- src/main/java/com/septemberhx/didi/controller/MainController.java
+++ src/main/java/com/septemberhx/didi/controller/MainController.java
@@ -1,7 +1,10 @@
 package com.septemberhx.didi.controller;
 
 import com.septemberhx.common.bean.MResponse;
+import com.septemberhx.didi.utils.MBaseUtils;
 import com.septemberhx.mclient.annotation.MFuncDescription;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,10 +20,20 @@ import javax.servlet.http.HttpServletRequest;
 @Controller
 public class MainController {
 
+    private Logger logger = LogManager.getLogger(this);
+
     @PostMapping(path = "/taxi")
     @ResponseBody
     @MFuncDescription(value = "taxi", level = 1)
     public MResponse weather(@RequestBody MResponse params, HttpServletRequest request) {
-        return MResponse.successResponse();
+
+        boolean r = MBaseUtils.verDepRequest("weather", 19, request, logger)
+                && MBaseUtils.verDepRequest("navigation", 9, request, logger)
+                && MBaseUtils.verDepRequest("pay", 20, request, logger);
+
+        if (!r) {
+            return MResponse.failResponse();
+        }
+        return MBaseUtils.generateResInKBSize(16);
     }
 }
